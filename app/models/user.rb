da96eb8f69c7e4c3d37ffad324516db2829019a5
Ashley Follen <ashleyfollen@outlook.com>
2021-09-10 11:02:13 -0600

    submission for deadline

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ActiveRecord::Base
-    
+    has_many :reviews
+    has_many :products, through: :reviews 
+
+    def favorite_product
+        favorite = self.reviews.order(star_rating: :desc).first
+        favorite.product.name
+    end
+
+    def remove_reviews(product)
+        bad_reviews = self.reviews.where("product_id = ?", product.id)
+        bad_reviews.destroy_all
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 puts "Deleting old data..."
 Product.destroy_all
 User.destroy_all
+Review.destroy_all
 
 puts "Creating users..."
 user1 = User.create(name: Faker::Name.name)
@@ -17,10 +18,37 @@ product4 = Product.create(name: "Ballpoint Pens", price: 2)
 product5 = Product.create(name: "Scotch Tape", price: 3)
 
 puts "Creating reviews..."
-# *****************************************************************
-# * TODO: create reviews! Remember, a review belongs to a product *
-# * and a review belongs to an user.                              *
-# *****************************************************************
-# Create Reviews Here
+review1 = Review.create(star_rating: 5, comment: "Love it", product_id: product1.id, user_id: user1.id)
+review2 = Review.create(star_rating: 4, comment: "Like it", product_id: product2.id, user_id: user2.id)
+review3 = Review.create(star_rating: 3, comment: "It's fine", product_id: product3.id, user_id: user3.id)
+review4 = Review.create(star_rating: 1, comment: "Hate it", product_id: product4.id, user_id: user1.id)
+review5 = Review.create(star_rating: 1, comment: "Yuck", product_id: product3.id, user_id: user3.id)
 
 puts "Seeding done!"
+
+# Testing
+# puts
+# puts review1.user.name
+# puts
+# puts review2.product.name
+# puts 
+# puts product1.reviews
+# puts 
+# puts product1.users
+# puts
+# puts user1.reviews
+# puts
+# puts user1.products
+# puts
+puts review1.print_review
+puts
+puts product1.leave_review(user2, 4, "Pretty cool")
+puts
+puts product1.print_all_reviews
+puts
+puts product1.average_rating
+puts
+puts user1.favorite_product
+puts 
+puts user3.remove_reviews(product3)
+
